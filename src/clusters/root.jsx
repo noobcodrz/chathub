d505f62d93507b4729c7838894fe0428aa7d42be
Keshav Garg <keshav2003garg@gmail.com>
2023-06-15 14:52:52 +0530

    Folder Structure for Navigation

--- src/clusters/root.jsx
+++ src/clusters/root.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-
+import Chat from '../screens/Home/HomeScreens/Chat';
 const Tab = createBottomTabNavigator();
 export default function Root() {
 	return (
-		<View>
-			<Text>Root</Text>
-		</View>
+		<>
+			<Chat />
+		</>
 	)
 }
