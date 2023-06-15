e0ee40ba2a5c67951de2798887cfb147bac076f8
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-06-15 19:36:33 +0530

    Auth done

--- src/clusters/root.jsx
+++ src/clusters/root.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../screens/Auth/Login';
+import Register from '../screens/Auth/Register';
 import Chat from '../screens/Home/HomeScreens/Chat';
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 export default function Root() {
-	return (
-		<>
-			<Chat />
-		</>
-	)
+  return (
+    <>
+      {/* <Chat /> */}
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Register">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
--- src/screens/Auth/Register.jsx
+++ src/screens/Auth/Register.jsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-export default function Register() {
+import { View, TextInput, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import Login from './Login';
+  const Register = ({navigation}) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleRegister = () => {
+      // Handle registration logic here
+      console.log('Registering...');
+      console.log('Name:', name);
+      console.log('Email:', email);
+      console.log('Password:', password);
+    };
     return (
-        <View>
-            <Text>Register</Text>
+        <View className='bg-green flex-1 justify-center align-center'>
+       <View className=' flex-1 justify-center items-center bg-white p-6'>
+      <Text className='text-3xl font-semibold  mb-4 text-green-700'>Register</Text>
+      <TextInput
+        className='w-full h-10 bg-gray-100 mb-2 px-2 hover:border-green-800 '
+        placeholder="Name"
+        onChangeText={(text) => setName(text)}
+      />
+      <TextInput
+        className='w-full h-10 bg-gray-100 mb-2 px-2'
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        className='w-full h-10 bg-gray-100 mb-2 px-2'
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      {/* <Button title="Register" onPress={handleRegister}/> */}
+        <View className="flex-13 flex-row gap-2">
+          <Button color="#17594A" title="Register" onPress={handleRegister} />
+          <Text onPress={()=>{navigation.navigate('Login')}} className="text-dGreen cursor-pointer">Login?</Text>
         </View>
+        </View>
+    </View>
     )
-}
+    }
+    export default Register;
