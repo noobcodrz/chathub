00e85d7207b27a694d9e71196bb01cc7a9757a8b
Keshav Garg <keshav2003garg@gmail.com>
2023-06-15 19:30:15 +0530

    Enhanced Chat UI

--- src/clusters/root.jsx
+++ src/clusters/root.jsx
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Chat from '../screens/Home/HomeScreens/Chat';
+
 const Tab = createBottomTabNavigator();
 export default function Root() {
 	return (
 		<>
-			<Chat />
+			
 		</>
 	)
 }
