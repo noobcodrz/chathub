55af09e62499f33cfc26eabbbd56fe17f7977e95
pranjal-trvd <115395810+pranjal-trvd@users.noreply.github.com>
2023-06-15 19:30:59 +0530

    Enhanced chatlist ui

--- src/clusters/root.jsx
+++ src/clusters/root.jsx
@@ -7,7 +7,6 @@ const Tab = createBottomTabNavigator();
 export default function Root() {
 	return (
 		<>
-			<ChatList />
 		</>
 	)
 }
--- src/screens/Home/HomeScreens/ChatList.jsx
+++ src/screens/Home/HomeScreens/ChatList.jsx
@@ -1,16 +1,25 @@
 import { View, Text, TextInput,Image } from 'react-native'
 import React from 'react'
+import SLI from 'react-native-vector-icons/dist/SimpleLineIcons'
 
-const Card = ({time,name})=> {
+const Card = ({time,name,msg})=> {
     return (
-        <View className="flex">
+        <View className="flex flex-row mt-4 pb-3 mx-1">
             <View>
-                <Image source={{uri: 'https://imgd.aeplcdn.com/1280x720/n/cw/ec/44686/activa-6g-right-front-three-quarter.jpeg?q=80'}}
-                className="w-5 h-5" /> 
+                <Image source={require("../../../../assets/images/avatar.png")}
+                className="w-14 h-14" />
             </View>
-            <View className="flex flex-row justify-between">
-                <Text >{name}</Text>
-                <Text>{time}</Text>
+            <View className="flex-1 justify-start">
+                <View className="flex flex-row justify-between">
+                    <Text className="text-lg text-[#262D25] font-medium">{name}</Text>
+                    <Text className="mt-[2]">{time}</Text>
+                </View>
+                <View>
+                    <Text>
+                        <Text className="font-medium">You: </Text>
+                        {msg}
+                    </Text>
+                </View>
             </View>
         </View>
     )
@@ -18,17 +27,24 @@ const Card = ({time,name})=> {
 
 export default function ChatList() {
     return (
-        <View className="flex m-4 ">
-            <View className="flex flex-row justify-between m-2">
-                <Text className="text-lg text-[#262D25]">Messages</Text>
-                <Text className="text-lg text-[#262D25]">Icons</Text>
-            </View>
-            <View>
-                <TextInput className="bg-[#F6F6F6] mx-3 mt-2" placeholder='Search' />
-            </View>
+        <View className="flex-1 bg-[#FFF]">
+            <View className="flex m-3  ">
+                <View className="flex flex-row justify-between m-2 items-center">
+                    <Text className="text-lg text-[#262D25] font-bold ">Messages</Text>
+                    <SLI name='options-vertical' size={20} />
+                </View>
+                <View>
+                    <TextInput className="bg-[#F6F6F6] m-2 px-2" placeholder='Search' />
+                </View>
 
-            <View>
-                <Card time="1hr ago" name="Pranjal" />
+                <View className="flex">
+                    <Text className="text-[#B2B2B2] mt-2 mx-2">Joined</Text>
+                    <Card time="1hr ago" name="Pranjal" msg="hello" />
+                    
+                    <Card time="1hr ago" name="Pranjal" msg="bye" />
+                    <Text className="text-[#B2B2B2] mt-2 mx-2">Recommended</Text>
+                    <Card time="1hr ago" name="Pranjal" msg="see you soon" />
+                </View>
             </View>
         </View>
     )
